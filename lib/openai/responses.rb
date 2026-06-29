78268a9317e769c8edeadd5d4021abbe384ac11d
Justin D. Harris <1594505+juharris@users.noreply.github.com>
2026-06-29 10:39:14 -0400

    [responses] Add extra_headers (#9)
    Let in client.rb

--- lib/openai/responses.rb
+++ lib/openai/responses.rb
@@ -4,20 +4,24 @@ module OpenAI
       @client = client
     end
 
-    def create(parameters: {})
-      @client.json_post(path: "/responses", parameters: parameters)
+    def create(parameters: {}, extra_headers: {})
+      @client.json_post(path: "/responses", parameters: parameters, extra_headers: extra_headers)
     end
 
-    def retrieve(response_id:)
-      @client.get(path: "/responses/#{response_id}")
+    def retrieve(response_id:, extra_headers: {})
+      @client.get(path: "/responses/#{response_id}", extra_headers: extra_headers)
     end
 
-    def delete(response_id:)
-      @client.delete(path: "/responses/#{response_id}")
+    def delete(response_id:, extra_headers: {})
+      @client.delete(path: "/responses/#{response_id}", extra_headers: extra_headers)
     end
 
-    def input_items(response_id:, parameters: {})
-      @client.get(path: "/responses/#{response_id}/input_items", parameters: parameters)
+    def input_items(response_id:, parameters: {}, extra_headers: {})
+      @client.get(
+        path: "/responses/#{response_id}/input_items",
+        parameters: parameters,
+        extra_headers: extra_headers
+      )
     end
   end
 end
